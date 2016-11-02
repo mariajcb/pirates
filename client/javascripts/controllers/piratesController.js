27e1bf7b734b037e46ee93e5fda310738d8953bc
mariajcb <bogomazm@gmail.com>
2016-11-02 15:53:33 -0600

    refactor file structure

--- client/javascripts/controllers/piratesController.js
+++ client/javascripts/controllers/piratesController.js
@@ -10,14 +10,13 @@ app.controller("PiratesController", ['$scope', 'PiratesService', function($scope
       })
 
     $scope.submitPirate = function(){
-      let newPirate = {}
-      newPirate.first_name = $scope.pirate.data.first_name;
-      newPirate.last_name = $scope.pirate.data.last_name;
-      newPirate.image_url = $scope.pirate.data.image_url;
-      console.log('LOGGING NEW PIRATE', newPirate);
-      PiratesService.create(newPirate)
+      // let newPirate = {}
+      // newPirate.first_name = $scope.pirate.data.first_name;
+      // newPirate.last_name = $scope.pirate.data.last_name;
+      // newPirate.image_url = $scope.pirate.data.image_url;
+      PiratesService.create($scope.pirate.data)
       .then(function() {
-        $scope.view.pirates.push(newPirate);
+        $scope.view.pirates.push($scope.pirate.data);
         $scope.pirate.data = {};
         $scope.newPirate.$setPristine();
         })
--- server/app.js
+++ server/app.js
@@ -10,8 +10,8 @@ var api = require('./routes/api');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -19,7 +19,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../client')));
 
 app.use('/api/pirates', api)
 
@@ -37,10 +37,10 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
+    // res.render('error', {
+    //   message: err.message,
+    //   error: err
+    // });
   });
 }
 
@@ -48,10 +48,10 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  // res.render('error', {
+  //   message: err.message,
+  //   error: {}
+  // });
 });
 
 
